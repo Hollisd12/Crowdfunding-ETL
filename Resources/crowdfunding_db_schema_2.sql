fed5fe912a69135e94a7823d9e2442803a447449
Hollisd12 <delilah.hollis@gmail.com>
2022-12-15 20:37:34 -0600

    Challenge Commit

---
+++ Resources/crowdfunding_db_schema_2.sql
@@ -0,0 +1,42 @@
+campaign
+-
+cf_id int pk fk -< backers.cf_id
+contact_id int fk - contacts.contact_id
+company_name varchar(100)
+description text
+goal numeric(10,2)
+pledged numeric(10,2)
+outcome varchar(50)
+backers_count int
+country varchar(10)
+currency varchar(10)
+launch_date date
+end_date date
+category_id varchar(10) fk >- category.category_id
+subcategory_id varchar(10) fk >- subcategory.subcategory_id
+
+
+category
+-
+category_id varchar(10) pk
+category_name varchar(50)
+
+subcategory
+-
+subcategory_id varchar(10) pk
+subcategory_name varchar(50)
+
+contacts
+-
+contact_id int pk
+first_name varchar(50)
+last_name varchar(50)
+email varchar(100)
+
+backers
+-
+backer_id vachar pk
+cf_id int fk 
+first_name vachar(50)
+last_name vachar(50)
+email vachar(100)
